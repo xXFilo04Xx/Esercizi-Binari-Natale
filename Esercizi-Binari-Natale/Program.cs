a178f11fd8983d60361a68a2b8eaa18eae65554f
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-12-20 13:24:34 +0100

    ho aggiunto un commento alla riga 10 e 11

--- Esercizi-Binari-Natale/Program.cs
+++ Esercizi-Binari-Natale/Program.cs
@@ -7,15 +7,15 @@ namespace Esercizi_Binari_Natale
         static void Main(string[] args)
         {
             int a = int.MaxValue;
-            Console.WriteLine(a);
-            Console.WriteLine(a + 1);
+            Console.WriteLine(a); //viene stampato il numero massimo che un INT può contenere
+            Console.WriteLine(a + 1); //il programma va in overflow perchè viene aggiunto un uno al numero massimo che 1 INT può contenere
             int b = -128;
             int c = -b;
             Console.WriteLine(Convert.ToString(b, 2));
             Console.WriteLine(Convert.ToString(c, 2));
             int value = 31;
             int somma = value;
-            value = ~value + 1;// NOT
+            value = ~value + 1;
             //value = value+1;
             somma = somma + value;
             Console.WriteLine($"{value} e {somma}");

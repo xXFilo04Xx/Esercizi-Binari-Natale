c3c626a4f612a5e31ad8abbc5215f65b283eee9c
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-12-20 13:17:30 +0100

    Add project files.

---
+++ Esercizi-Binari-Natale/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Esercizi_Binari_Natale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.MaxValue;
+            Console.WriteLine(a);
+            Console.WriteLine(a + 1);
+            int b = -128;
+            int c = -b;
+            Console.WriteLine(Convert.ToString(b, 2));
+            Console.WriteLine(Convert.ToString(c, 2));
+            int value = 31;
+            int somma = value;
+            value = ~value + 1;// NOT
+            //value = value+1;
+            somma = somma + value;
+            Console.WriteLine($"{value} e {somma}");
+            long number = 55555;
+            Console.WriteLine(Convert.ToString(number, 2));
+            short number1 = (short)number;
+            Console.WriteLine(number1);
+
+
+
+
+        }
+    }
+}
